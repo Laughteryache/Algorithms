6e49f2fac67660b8849a08f09b0716093bd9f229
Laughteryache <kostrominefim2@gmail.com>
2024-03-20 09:02:41 +0500

    Added sort ascending

--- sort_ascending.py
+++ sort_ascending.py
@@ -24,6 +24,3 @@ def sort_ascending(arr: list[int]) -> list[int]:
 # arr = [-3, -1, -4, -1, -5, -9, -2, -6, -5, -3, -5]
 # arr = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
 # arr = [5]
-
-print(sort_ascending([-3, -1, -4, -1, -5, -9, -2, -6, -5, -3, -5]))
-
